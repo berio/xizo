da990dce778872dfb31d4bdb30c0d43161e450be
berio <beries@gmail.com>
2022-01-20 12:56:45 +0100

    Soporte para favicon svg

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-      <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-   	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+			<link rel="icon" type="image/svg+xml" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.svg">
+			<link rel="alternate icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico">
+			<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/safari-pinned-tab.svg" color="#ff8a01">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
